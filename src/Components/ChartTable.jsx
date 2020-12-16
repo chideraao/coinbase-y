0e3685759d1f5d4228cb9778007944b2c85fa8e0
Chidera <okekechidera97@gmail.com>
2020-12-17 00:09:05 +0100

    header done

---
+++ src/Components/ChartTable.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ChartTable() {
+	return <div></div>;
+}
+
+export default ChartTable;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+function Header() {
+	return (
+		<div className="header">
+			<div className="navbar flex container">
+				<div className="flex">
+					<a href="/">
+						{" "}
+						<h1 className="logo">coinbase</h1>
+					</a>
+					<nav>
+						<ul>
+							<li>
+								<a href="/prices">Prices</a>
+							</li>
+							<li>
+								<a href="/products">Products</a>
+							</li>
+							<li>
+								<a href="/company">Company</a>
+							</li>
+							<li>
+								<a href="/crypto">Crypto</a>
+							</li>
+						</ul>
+					</nav>
+				</div>
+				<div className="auth">
+					<a href="" className="btn">
+						Sign In
+					</a>
+					<a href="" className="btn btn-outline">
+						Get Started
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Header;
