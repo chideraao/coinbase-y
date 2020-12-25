3dcee654b46e7e8251c90ca0d7af8cf80f70f1b4
Chidera <okekechidera97@gmail.com>
2020-12-25 18:34:27 +0100

    still on about

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -41,11 +41,11 @@ function About() {
 				<hr />
 			</section>
 			<section className="main">
-				<div className="container">
+				<div className="container grid">
 					<div className="main-text">
 						<h2>Basecoin is how the world uses crypto</h2>
 						<p>
-							In 2012, Coinbase had the radical idea that anyone — anywhere —
+							In 2012, Basecoin had the radical idea that anyone — anywhere —
 							should be able to easily and securely access Bitcoin. We see
 							cryptocurrency as the future of money and a catalyst for creating
 							an open financial system around the world.
@@ -56,16 +56,33 @@ function About() {
 						</p>
 					</div>
 					<div className="main-stats">
-						<h2>
-							Stats
-							<h3></h3>
-							<h3></h3>
-							<h3></h3>
-							<h3></h3>
-							<h3></h3>
-						</h2>
+						<h2>Stats</h2>
+						<h3>
+							35M+
+							<h4>VERIFIED USERS</h4>
+						</h3>
+						<h3>
+							$320B+
+							<h4>TOTAL VOLUME TRADED</h4>
+						</h3>
+						<h3>
+							$25B+
+							<h4>ASSETS ON PLATFORM</h4>
+						</h3>
+						<h3>
+							100+
+							<h4>COUNTRIES</h4>
+						</h3>
+						<h3>
+							1000+
+							<h4>EMPLOYEES</h4>
+						</h3>
 					</div>
 				</div>
+			</section>
+			<hr />
+			<section className="execs">
+				<div className="container"></div>
 			</section>
 			<AboutFooter />
 		</div>
