818ae36fa9ee07ae7d8f67006708748f1085a89f
Chidera Okeke <okekechidera97@gmail.com>
2021-10-05 15:46:42 +0100

    minor typo

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 
 export function Loader() {
-	return (
-		<div className="loader">
-			<div>
-				<div className="loading-1"></div>
-				<div>Powered by CoinGecko API, Nomics API and ipgeolocation API</div>
-				<div className="post-script">
-					PS: Turn of blockers on your browser, if any.
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="loader">
+      <div>
+        <div className="loading-1"></div>
+        <div>Powered by CoinGecko API, Nomics API and ipgeolocation API</div>
+        <div className="post-script">
+          PS: Turn off blockers on your browser, if any.
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export function Loading() {
-	return (
-		<div className="loading-div">
-			<div className="loading-2"></div>
-		</div>
-	);
+  return (
+    <div className="loading-div">
+      <div className="loading-2"></div>
+    </div>
+  );
 }
