7481683cee965ba032e9b1b726ea84a4801f3194
Chidera Okeke <okekechidera97@gmail.com>
2022-05-08 14:01:26 +0100

    minor refactoring

--- src/Components/AboutHeader.jsx
+++ src/Components/AboutHeader.jsx
@@ -2,77 +2,77 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import { useMediaQuery } from "../customHook/UseMediaQuery";
+import { useMediaQuery } from "../helpers/UseMediaQuery";
 import { HeaderContext } from "../State/HeaderContext";
 
 function AboutHeader() {
-	const [menuClick, setMenuClick] = useContext(HeaderContext);
+  const [menuClick, setMenuClick] = useContext(HeaderContext);
 
-	let isPageSmall = useMediaQuery("(max-width: 800px)");
+  let isPageSmall = useMediaQuery("(max-width: 800px)");
 
-	/**handling click events for page resize */
-	const handleClick = () => {
-		setMenuClick(!menuClick);
-	};
+  /**handling click events for page resize */
+  const handleClick = () => {
+    setMenuClick(!menuClick);
+  };
 
-	return (
-		<div className="about-header" id="header">
-			{isPageSmall && (
-				<div className="narrow-header flex">
-					<div className="">
-						<Link to="/">
-							<h1 className="logo">basecoin</h1>
-						</Link>
-					</div>
-					<div className="menu-bar" onClick={handleClick}>
-						<FontAwesomeIcon
-							className="font-awesome"
-							fontWeight="light"
-							icon="bars"
-							size="2x"
-						/>
-					</div>
-				</div>
-			)}
-			{!isPageSmall && (
-				<div className="navbar flex container">
-					<div className="flex">
-						<Link to="/">
-							{" "}
-							<h1 className="logo">basecoin</h1>
-						</Link>
-					</div>
-					<div className="auth">
-						<nav className="nav">
-							<ul>
-								<li>
-									<Link className="products" to="/products">
-										Products
-									</Link>
-								</li>
-								<li>
-									<Link className="help" to="/help">
-										Help
-									</Link>
-								</li>
-								<li>
-									<Link className="prices" to="/prices">
-										Prices
-									</Link>
-								</li>
-							</ul>
-						</nav>
-						<a href="" className="btn">
-							Sign In
-						</a>
-						<a href="" className="btn btn-outline">
-							Sign Up
-						</a>
-					</div>
-				</div>
-			)}
-		</div>
-	);
+  return (
+    <div className="about-header" id="header">
+      {isPageSmall && (
+        <div className="narrow-header flex">
+          <div className="">
+            <Link to="/">
+              <h1 className="logo">basecoin</h1>
+            </Link>
+          </div>
+          <div className="menu-bar" onClick={handleClick}>
+            <FontAwesomeIcon
+              className="font-awesome"
+              fontWeight="light"
+              icon="bars"
+              size="2x"
+            />
+          </div>
+        </div>
+      )}
+      {!isPageSmall && (
+        <div className="navbar flex container">
+          <div className="flex">
+            <Link to="/">
+              {" "}
+              <h1 className="logo">basecoin</h1>
+            </Link>
+          </div>
+          <div className="auth">
+            <nav className="nav">
+              <ul>
+                <li>
+                  <Link className="products" to="/products">
+                    Products
+                  </Link>
+                </li>
+                <li>
+                  <Link className="help" to="/help">
+                    Help
+                  </Link>
+                </li>
+                <li>
+                  <Link className="prices" to="/prices">
+                    Prices
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+            <a href="" className="btn">
+              Sign In
+            </a>
+            <a href="" className="btn btn-outline">
+              Sign Up
+            </a>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default AboutHeader;
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,131 +2,131 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
-import { useMediaQuery } from "../customHook/UseMediaQuery";
+import { useMediaQuery } from "../helpers/UseMediaQuery";
 import { HeaderContext } from "../State/HeaderContext";
 
 function Header() {
-	const [mouseOver, setMouseOver] = useState(false);
+  const [mouseOver, setMouseOver] = useState(false);
 
-	const [menuClick, setMenuClick] = useContext(HeaderContext);
+  const [menuClick, setMenuClick] = useContext(HeaderContext);
 
-	/**handling mouse events */
-	const handleMouseOver = () => {
-		setMouseOver(true);
-	};
+  /**handling mouse events */
+  const handleMouseOver = () => {
+    setMouseOver(true);
+  };
 
-	const handleMouseLeave = () => {
-		setMouseOver(false);
-	};
+  const handleMouseLeave = () => {
+    setMouseOver(false);
+  };
 
-	/**handling click events for page resize */
-	const handleClick = () => {
-		setMenuClick(!menuClick);
-	};
+  /**handling click events for page resize */
+  const handleClick = () => {
+    setMenuClick(!menuClick);
+  };
 
-	/** using custom hook for page resize */
-	let isPageSmall = useMediaQuery("(max-width: 800px)");
+  /** using custom hook for page resize */
+  let isPageSmall = useMediaQuery("(max-width: 800px)");
 
-	return (
-		<div className="header" id="header">
-			{isPageSmall ? (
-				<div className="narrow-header flex">
-					<div className="flex">
-						<Link to="/">
-							<h1 className="logo">basecoin</h1>
-						</Link>
-					</div>
-					<div className="flex">
-						<Link to="/earn" className="btn green-btn">
-							Get started
-						</Link>
-						<div className="menu-bar" onClick={handleClick}>
-							<FontAwesomeIcon
-								className="font-awesome"
-								fontWeight="light"
-								icon="bars"
-								size="2x"
-							/>
-						</div>
-					</div>
-				</div>
-			) : (
-				""
-			)}
+  return (
+    <div className="header" id="header">
+      {isPageSmall ? (
+        <div className="narrow-header flex">
+          <div className="flex">
+            <Link to="/">
+              <h1 className="logo">basecoin</h1>
+            </Link>
+          </div>
+          <div className="flex">
+            <Link to="/earn" className="btn green-btn">
+              Get started
+            </Link>
+            <div className="menu-bar" onClick={handleClick}>
+              <FontAwesomeIcon
+                className="font-awesome"
+                fontWeight="light"
+                icon="bars"
+                size="2x"
+              />
+            </div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
 
-			{!isPageSmall ? (
-				<div className="navbar flex container">
-					<div className="flex">
-						<Link to="/">
-							<h1 className="logo">basecoin</h1>
-						</Link>
+      {!isPageSmall ? (
+        <div className="navbar flex container">
+          <div className="flex">
+            <Link to="/">
+              <h1 className="logo">basecoin</h1>
+            </Link>
 
-						<nav className="nav">
-							<ul>
-								<Link className="prices" to="/prices">
-									<li>Prices</li>
-								</Link>
-								<Link className="learn-head" to="/learn">
-									<li>Learn</li>
-								</Link>
-								<Link
-									to="#"
-									className="company"
-									onMouseOver={handleMouseOver}
-									onMouseLeave={handleMouseLeave}
-								>
-									<li>
-										Company &nbsp;
-										<FontAwesomeIcon
-											className="font-awesome-header"
-											icon="chevron-down"
-											size="2x"
-										/>
-									</li>
-									{mouseOver ? (
-										<div
-											className="mouse-over card"
-											onMouseOver={handleMouseOver}
-										>
-											<ul className="flex-column">
-												<Link to="/about">
-													<li>About us</li>
-												</Link>
-												<Link to="/press">
-													<li>Press</li>
-												</Link>
-												<Link to="/careers">
-													<li>Careers</li>
-												</Link>
-											</ul>
-										</div>
-									) : (
-										""
-									)}
-								</Link>
-								<Link to="/earn" className="crypto">
-									<li>Earn crypto</li>
-								</Link>
-							</ul>
-						</nav>
-						<Link to="/earn" className="btn green-btn">
-							Get $40+
-						</Link>
-					</div>
-					<div className="auth">
-						<a href="#" className="btn">
-							Sign In
-						</a>
-						<a href="#" className="btn btn-outline">
-							Get Started
-						</a>
-					</div>
-				</div>
-			) : (
-				""
-			)}
-		</div>
-	);
+            <nav className="nav">
+              <ul>
+                <Link className="prices" to="/prices">
+                  <li>Prices</li>
+                </Link>
+                <Link className="learn-head" to="/learn">
+                  <li>Learn</li>
+                </Link>
+                <Link
+                  to="#"
+                  className="company"
+                  onMouseOver={handleMouseOver}
+                  onMouseLeave={handleMouseLeave}
+                >
+                  <li>
+                    Company &nbsp;
+                    <FontAwesomeIcon
+                      className="font-awesome-header"
+                      icon="chevron-down"
+                      size="2x"
+                    />
+                  </li>
+                  {mouseOver ? (
+                    <div
+                      className="mouse-over card"
+                      onMouseOver={handleMouseOver}
+                    >
+                      <ul className="flex-column">
+                        <Link to="/about">
+                          <li>About us</li>
+                        </Link>
+                        <Link to="/press">
+                          <li>Press</li>
+                        </Link>
+                        <Link to="/careers">
+                          <li>Careers</li>
+                        </Link>
+                      </ul>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                </Link>
+                <Link to="/earn" className="crypto">
+                  <li>Earn crypto</li>
+                </Link>
+              </ul>
+            </nav>
+            <Link to="/earn" className="btn green-btn">
+              Get $40+
+            </Link>
+          </div>
+          <div className="auth">
+            <a href="#" className="btn">
+              Sign In
+            </a>
+            <a href="#" className="btn btn-outline">
+              Get Started
+            </a>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
 }
 
 export default Header;
